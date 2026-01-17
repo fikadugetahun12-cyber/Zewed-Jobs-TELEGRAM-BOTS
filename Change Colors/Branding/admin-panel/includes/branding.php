0255f0370ff749c76b7ce6784fd339960e0b48f5
fikadugetahun12-cyber <fikadugetahun12@gmail.com>
2026-01-17 11:34:12 +0300

    Create branding.php

---
+++ Change Colors/Branding/admin-panel/includes/branding.php
@@ -0,0 +1,7 @@
+$branding = [
+    'name' => 'Your Company Name',
+    'logo' => '../assets/images/your-logo.png',
+    'primary_color' => '#2c3e50',
+    'secondary_color' => '#3498db',
+    // ...
+];
